14f2917e9c26c655eb7a97211d18880c686eba8b
Doyeon04 <rlaehdus0411@naver.com>
2022-04-06 21:46:03 +0900

    chick, item 생성 후 충돌, 일정 시간 후 사라짐

--- Assets/Scripts/cshPlayerController.cs
+++ Assets/Scripts/cshPlayerController.cs
@@ -8,11 +8,34 @@ public class cshPlayerController : MonoBehaviour
     public float moveableRange = 5.5f; // 이동 가능한 범위
     public float power = 1000f; // CannonBall을 발사하는 힘
 
+    public GameObject cannonBall; //Player에서 발사할 CannonBall
+    public Transform spawnPoint; //Cannon 발사 지점
+
+
     void Update()
     {
         //Player 이동 (이동 범위를 movableRange로 제한)
         transform.Translate(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0, 0);
         transform.position
         = new Vector2(Mathf.Clamp(transform.position.x, -moveableRange, moveableRange), transform.position.y);
+
+        // 포탄 발사 기능 추가  
+        if (Input.GetKeyDown(KeyCode.Space))  //스페이스키 버튼 이벤트 처리
+        {
+            Shoot(); //Shoot함수 호출
+        }
+
     }
+    void Shoot()
+    {
+        //새 cannonBall을 생성하여 newBullet에 할당
+        GameObject newBullet = Instantiate(cannonBall, spawnPoint.position, Quaternion.identity) as GameObject;
+        // cannonBall을 지정된 위치에 지정된 방향으로 복제 
+        //newBullet의 Rigidbody2D를 참조하여 AddForce 함수로 물리적으로 발사
+
+        newBullet.GetComponent<Rigidbody2D>().AddForce(Vector3.up * power);
+    }
+
+
+
 }
